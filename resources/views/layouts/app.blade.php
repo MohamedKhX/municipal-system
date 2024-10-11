bb078dcddb8c8351910bdecb45d29163c2378285
MohamedKhX <muhamedkhx2@gmail.com>
2024-10-11 15:01:40 +0200

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ar" dir="rtl">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,30 +7,103 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+
+        <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href=" {{ asset('css/fontawsome.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('fonts/flaticon.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/meanmenu.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/owl.carousel.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/nice-select.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/owl.theme.default.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/magnific-popup.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/jquery-ui.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/odometer.min.css') }} ">
+        <link rel="stylesheet" href=" {{ asset('css/style.css') }}">
+        <link rel="stylesheet" href=" {{ asset('css/responsive.css') }} ">
+
+
+
+
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600;800&display=swap" rel="stylesheet">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <style>
+            body {
+                font-family: 'Rubik', sans-serif !important;
+            }
+        </style>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+   <body>
+       <div class="navbar-area">
+       <div class="main-responsive-nav">
+           <div class="container">
+               <div class="mobile-nav">
+                   <a href="index.html" class="logo"><img src="{{ asset('images/small-logo.png') }}" alt="logo"/></a>
+                   <ul class="menu-sidebar menu-small-device">
+                       <li>
+                           <button class="popup-button"><i class="fas fa-search"></i></button>
+                       </li>
+                       <li><a class="default-button" href="contact.html">احصل على عرض <i class="fas fa-angle-right"></i></a>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+       </div>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+       <div class="main-nav">
+           <div class="container">
+               <nav class="navbar navbar-expand-md navbar-light">
+                   <a class="navbar-brand" href="index.html">
+                       <img src="{{ asset('images/small-logo.png') }}" alt="logo"/>
+                   </a>
+                   <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                       <ul class="navbar-nav justify-content-center flex-grow-1">
+                           <li class="nav-item"><a href="contact.html" class="nav-link">الصفحة الرئيسية</a></li>
+                           <li class="nav-item"><a href="contact.html" class="nav-link">البلاغات</a></li>
+                           <li class="nav-item"><a href="contact.html" class="nav-link">الأخبار</a></li>
+                           <li class="nav-item"><a href="contact.html" class="nav-link">الخدمات</a></li>
+                           <li class="nav-item"><a href="contact.html" class="nav-link">اتصل بنا</a></li>
+                       </ul>
+                       <div class="menu-sidebar">
+                           <ul>
+                               <li>
+                                   <a class="default-button" href="contact.html">
+                                       <i class="fas fa-sign-in-alt"></i>
+                                       تسجيل دخول
+                                   </a>
+                               </li>
+                           </ul>
+                       </div>
+                   </div>
+               </nav>
+           </div>
+       </div>
+   </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+       <div>
+           {{ $slot }}
+       </div>
+
+       <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+       <script src=" {{ asset('js/jquery.min.js') }}"></script>
+       <script src=" {{ asset('js/jquery-ui.min.js') }}"></script>
+       <script src=" {{ asset('js/bootstrap.bundle.min.js') }}"></script>
+       <script src=" {{ asset('js/meanmenu.js') }}"></script>
+       <script src=" {{ asset('js/owl.carousel.min.js') }} "></script>
+       <script src=" {{ asset('js/magnific-popup.min.js') }}"></script>
+       <script src=" {{ asset('js/TweenMax.js') }}"></script>
+       <script src=" {{ asset('js/nice-select.min.js') }} "></script>
+       <script src=" {{ asset('js/form-validator.min.js') }} "></script>
+       <script src=" {{ asset('js/contact-form-script.js') }}"></script>
+       <script src=" {{ asset('js/ajaxchimp.min.js') }} "></script>
+       <script src=" {{ asset('js/owl.carousel2.thumbs.min.js') }}"></script>
+       <script src=" {{ asset('js/appear.min.js') }} "></script>
+       <script src=" {{ asset('js/odometer.min.js') }} "></script>
+       <script src=" {{ asset('js/custom.js') }}"></script>
+   </body>
 </html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,273 @@
+<x-app-layout>
+
+    <section class="home-banner ptb-100 footer">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="banner-text-area">
+                        <h6>اكتشف المدينة</h6>
+                        <h1>نحن نطور مدينة مستدامة</h1>
+                        <p>لوريم إيبسوم دولور سيت أميت، كونسيكتيتور أديبيسكنغ، فاسيليس هييندريت لوريم دولور سيت أميت ماجنا
+                            نيب نيك أورنا إن نيسي نيكي أليكيت.</p>
+                        <a class="default-button" href="about.html">تعرف على المزيد</a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="home-banner-area">
+                        <img src="https://scontent.fmji3-1.fna.fbcdn.net/v/t39.30808-6/311662218_3216886508641706_1499843151251689716_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=zDhDHM1Oi0AQ7kNvgEn1j64&_nc_ht=scontent.fmji3-1.fna&_nc_gid=AwLyQdvx5Wc7hnbpe1JBBad&oh=00_AYB9TMqtA3qSdt0HDaeL23f_hUt6fepkVPpQT6SVGlnlKg&oe=670EB80B" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="reports ptb-100 bg-f9fbfe">
+        <div class="container">
+            <div class="default-section-title default-section-title-middle">
+                <h3>البلاغات البلدية</h3>
+                <p>هنا تجد أحدث البلاغات المتعلقة بالخدمات البلدية والمشاكل التي تحتاج إلى المتابعة. تساهم هذه البلاغات في تحسين مستوى الخدمة ومعالجة القضايا في الوقت المناسب.</p>
+            </div>
+            <div class="section-content">
+                <div class="row justify-content-center">
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="blog-card">
+                            <div class="blog-card-img">
+                                <a href="blog-details.html"><img style="height: 300px !important;" src="https://images.unsplash.com/photo-1515162816999-a0c47dc192f7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG90aG9sZSUyMGluJTIwdGhlJTIwcm9hZHxlbnwwfHwwfHx8MA%3D%3D" alt="image"></a>
+                            </div>
+                            <div class="blog-card-text-area">
+                                <div class="blog-date">
+                                    <ul>
+                                        <li><i class="far fa-calendar-alt"></i> 01 نوفمبر 2024</li>
+                                    </ul>
+                                </div>
+                                <h4><a href="blog-details.html">بلاغ عن حفرة في الطريق</a></h4>
+                                <p>تم تقديم بلاغ عن وجود حفرة كبيرة في أحد الشوارع الرئيسية، تتطلب التدخل العاجل.</p>
+                                <a class="read-more-btn" href="blog-details.html">اقرأ المزيد</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="blog-card">
+                            <div class="blog-card-img">
+                                <a href="blog-details.html"><img style="height: 300px !important;" src="https://images.unsplash.com/photo-1620631049586-2670d39a1cef?q=80&w=2586&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image"></a>
+                            </div>
+                            <div class="blog-card-text-area">
+                                <div class="blog-date">
+                                    <ul>
+                                        <li><i class="far fa-calendar-alt"></i> 02 نوفمبر 2024</li>
+                                    </ul>
+                                </div>
+                                <h4><a href="blog-details.html">بلاغ عن تسرب مياه</a></h4>
+                                <p>بلاغ بوجود تسرب مياه في أحد الأحياء السكنية، تم إرسال الفريق المختص للتحقق من الأمر.</p>
+                                <a class="read-more-btn" href="blog-details.html">اقرأ المزيد</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="blog-card">
+                            <div class="blog-card-img">
+                                <a href="blog-details.html"><img style="height: 300px !important;" src="https://images.unsplash.com/photo-1530587191325-3db32d826c18?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image"></a>
+                            </div>
+                            <div class="blog-card-text-area">
+                                <div class="blog-date">
+                                    <ul>
+                                        <li><i class="far fa-calendar-alt"></i> 03 نوفمبر 2024</li>
+                                    </ul>
+                                </div>
+                                <h4><a href="blog-details.html">بلاغ عن نفايات غير مرفوعة</a></h4>
+                                <p>تم تقديم بلاغ عن تراكم النفايات في منطقة معينة، جارٍ التنسيق لرفعها بأسرع وقت ممكن.</p>
+                                <a class="read-more-btn" href="blog-details.html">اقرأ المزيد</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about pb-100 pt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <div class="about-img">
+                        <img class="a-img-1" src="assets/images/about/a1.jpg" alt="image">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <div class="why-we-text-area about-text-area-2">
+                        <div class="default-section-title">
+                            <span>التراخيص والتصاريح</span>
+                            <h3>منصة الخدمات البلدية لإصدار التراخيص والتصاريح</h3>
+                            <p>نحن نوفر حلولاً متكاملة لإصدار التراخيص والتصاريح بطريقة سهلة وسريعة تضمن الامتثال لجميع اللوائح القانونية.</p>
+                        </div>
+                        <div class="why-we-text-list">
+                            <h4>دورنا يشمل:</h4>
+                            <p>نسعى لضمان تقديم خدمات إصدار التراخيص والتصاريح بطريقة فعّالة، وفقاً لأعلى معايير الجودة.</p>
+                            <ul>
+                                <li>توفير إجراءات مبسطة ومرنة لإصدار التراخيص.</li>
+                                <li>ضمان الامتثال الكامل للأنظمة واللوائح.</li>
+                                <li>دعم المجتمع المحلي بتوفير خدمات ذات كفاءة عالية.</li>
+                            </ul>
+                            <a class="default-button mt-4" href="about.html">تعرف على المزيد</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="blog ptb-100 bg-f9fbfe">
+        <div class="container">
+            <div class="default-section-title default-section-title-middle">
+                <h3>آخر الأخبار والتحديثات</h3>
+                <p>لوريم إيبسوم دولور سيت أميت، كونسيكتيتور أديبيسكنغ إيليت، سيد دو إييوسمود تيمبور إنكيديدونت أوت
+                    لابوري إت دولوري ماغنا أليكوا كويز إيبسوم سوسبينديشي.</p>
+            </div>
+            <div class="section-content">
+                <div class="row justify-content-center">
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="blog-card">
+                            <div class="blog-card-img">
+                                <a href="blog-details.html"><img src="assets/images/blog/b1.jpg" alt="image"></a>
+                            </div>
+                            <div class="blog-card-text-area">
+                                <div class="blog-date">
+                                    <ul>
+                                        <li><i class="far fa-calendar-alt"></i> 01 نوفمبر 2024</li>
+                                    </ul>
+                                </div>
+                                <h4><a href="blog-details.html">الاستجابة لنصائح المواطنين</a></h4>
+                                <p>لوريم إيبسوم دولور أميت ماغنا سيت دولور سيت أميت كونسيكتيتور أديبيسكنغ دو إيليت لابوري.</p>
+                                <a class="read-more-btn" href="blog-details.html">اقرأ المزيد</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="blog-card">
+                            <div class="blog-card-img">
+                                <a href="blog-details.html"><img src="assets/images/blog/b2.jpg" alt="image"></a>
+                            </div>
+                            <div class="blog-card-text-area">
+                                <div class="blog-date">
+                                    <ul>
+                                        <li><i class="far fa-calendar-alt"></i> 02 نوفمبر 2024</li>
+                                    </ul>
+                                </div>
+                                <h4><a href="blog-details.html">برنامج مستشاري الإسكان</a></h4>
+                                <p>لوريم إيبسوم دولور أميت ماغنا سيت دولور سيت أميت كونسيكتيتور أديبيسكنغ دو إيليت لابوري.</p>
+                                <a class="read-more-btn" href="blog-details.html">اقرأ المزيد</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="blog-card">
+                            <div class="blog-card-img">
+                                <a href="blog-details.html"><img src="assets/images/blog/b3.jpg" alt="image"></a>
+                            </div>
+                            <div class="blog-card-text-area">
+                                <div class="blog-date">
+                                    <ul>
+                                        <li><i class="far fa-calendar-alt"></i> 03 نوفمبر 2024</li>
+                                    </ul>
+                                </div>
+                                <h4><a href="blog-details.html">الاستجابة للتقرير الوطني</a></h4>
+                                <p>لوريم إيبسوم دولور أميت ماغنا سيت دولور سيت أميت كونسيكتيتور أديبيسكنغ دو إيليت لابوري.</p>
+                                <a class="read-more-btn" href="blog-details.html">اقرأ المزيد</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="container">
+            <div class="footer-content ptb-100">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="footer-logo-area">
+                            <a href="index.html"><img src="assets/images/white-logo.png" alt="image"></a>
+                            <p>لوريم إيبسوم دولور سيت أميت، كونسيكتيتور أديبيسكنغ إيليت، تيمبور إنكيديدونت أوت لابوري
+                                إت دولوري ماغنا أليكوا كونسيكتيتور أديبيسكنغ إيليت سيد دو لابور.</p>
+                            <div class="footer-social-area">
+                                <ul>
+                                    <li><span>تابعنا: </span></li>
+                                    <li><a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="footer-links footer-contact">
+                            <h3>تواصل معنا</h3>
+                            <div class="footer-contact-card">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <h5>الموقع: </h5>
+                                <p><a href="https://goo.gl/maps/bc3qza49szqGNZt86" target="_blank">2976 طريق شروق الشمس، لاس فيغاس</a></p>
+                            </div>
+                            <div class="footer-contact-card">
+                                <i class="fas fa-envelope"></i>
+                                <h5>البريد الإلكتروني: </h5>
+                                <p><a href="../../cdn-cgi/l/email-protection.html#83ebe6efefecc3eee6e7f9ecade0ecee"><span
+                                            class="__cf_email__" data-cfemail="e68e838a8a89a68b83829c89c885898b">[البريد الإلكتروني محمي]</span></a>
+                                </p>
+                            </div>
+                            <div class="footer-contact-card">
+                                <i class="fas fa-phone-alt"></i>
+                                <h5>الهاتف: </h5>
+                                <p><a href="tel:+13454567877">+1-3454-5678-77</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="footer-links footer-quick-links">
+                            <h3>روابط سريعة</h3>
+                            <ul>
+                                <li><i class="fas fa-angle-right"></i> <a href="services.html">خدمة حكومية</a></li>
+                                <li><i class="fas fa-angle-right"></i> <a
+                                        href="https://templates.hibootstrap.com/medzo/default/tetrms.html">الشروط و الأحكام</a></li>
+                                <li><i class="fas fa-angle-right"></i> <a href="privacy.html">سياسة الخصوصية</a></li>
+                                <li><i class="fas fa-angle-right"></i> <a href="privacy.html">الوصول</a></li>
+                                <li><i class="fas fa-angle-right"></i> <a href="events.html">الفعاليات الأخيرة</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="footer-links footer-newsletter">
+                            <h3>اشترك</h3>
+                            <p>اشترك في نشرتنا الإخبارية للحصول على آخر التحديثات والأخبار!</p>
+                            <form class="newsletter-form" data-toggle="validator">
+                                <input type="email" class="input-newsletter form-control" placeholder="بريدك الإلكتروني"
+                                       name="EMAIL" required autocomplete="off">
+                                <button class="default-button news-btn">اشترك الآن</button>
+                                <div id="validator-newsletter" class="form-result"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <div class="popup">
+        <div class="popup-content">
+            <button class="close-btn" id="popup-close"><i class="fas fa-times"></i></button>
+            <form>
+                <div class="input-group search-box">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="go-top">
+        <i class="fas fa-chevron-up"></i>
+    </div>
+
+</x-app-layout>
