918e904558ea2643c1dbdfc8e845d07474e031d0
MohamedKhX <muhamedkhx2@gmail.com>
2025-02-01 14:33:30 +0200

    add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'mun' => \App\Http\Middleware\Municipal::class
         ]);
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
